f84a0d69f5b89ccd148072f0de9ab7979040c1c9
cong71925 <cong71925@gmail.com>
2024-01-16 15:44:32 +0800

    Feat: 新增AV1编码

--- src/preload/setting.ts
+++ src/preload/setting.ts
@@ -1,21 +1,184 @@
 import { ipcRenderer } from 'electron'
+import Ajv, { JSONSchemaType } from 'ajv'
 
-const defaultSetting: SettingOptions = {
-  encoder: 'libx265',
-  crf: 23,
-  preset: 'medium',
-  outputType: 'original',
-  outputWebpLossless: 0,
-  outputQualityLevel: 8
+const ajv = new Ajv({ useDefaults: true })
+export const settingSchema: JSONSchemaType<SettingOptions> = {
+  type: 'object',
+  properties: {
+    encoder: {
+      type: 'string',
+      enum: ['hevc', 'av1'],
+      default: 'hevc'
+    },
+    hardware: {
+      type: 'string',
+      enum: ['', 'amd', 'nvidia'],
+      default: ''
+    },
+    crf_libx265: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 51,
+      default: 18
+    },
+    preset_libx265: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 9,
+      default: 5
+    },
+    crf_hevc_amf: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 51,
+      default: 12
+    },
+    preset_hevc_amf: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 10,
+      default: 5
+    },
+    crf_hevc_nvenc: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 51,
+      default: 18
+    },
+    preset_hevc_nvenc: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 6,
+      default: 3
+    },
+    crf_libsvtav1: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 62,
+      default: 5
+    },
+    preset_libsvtav1: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 13,
+      default: 7
+    },
+    crf_av1_amf: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 255,
+      default: 12
+    },
+    preset_av1_amf: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 10,
+      default: 7
+    },
+    crf_av1_nvenc: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 51,
+      default: 18
+    },
+    preset_av1_nvenc: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 6,
+      default: 3
+    },
+    outputType: {
+      type: 'string',
+      enum: ['original', 'jpeg', 'png', 'webp'],
+      default: 'original'
+    },
+    outputWebpLossless: {
+      type: 'number',
+      enum: [0, 1],
+      default: 0
+    },
+    outputQualityLevel: {
+      type: 'number',
+      multipleOf: 1,
+      minimum: 0,
+      maximum: 9,
+      default: 8
+    }
+  },
+  required: [
+    'encoder',
+    'hardware',
+    'crf_libx265',
+    'preset_libx265',
+    'crf_hevc_amf',
+    'preset_hevc_amf',
+    'crf_hevc_nvenc',
+    'preset_hevc_nvenc',
+    'crf_libsvtav1',
+    'preset_libsvtav1',
+    'crf_av1_amf',
+    'preset_av1_amf',
+    'crf_av1_nvenc',
+    'preset_av1_nvenc',
+    'outputType',
+    'outputWebpLossless',
+    'outputQualityLevel'
+  ]
 }
+const settingValidate = ajv.compile(settingSchema)
+const defaultSetting = Object.fromEntries(
+  Object.entries(settingSchema.properties as unknown as JSONSchemaType<SettingOptions>).map(
+    ([key, value]) => [key, value.default]
+  )
+) as SettingOptions
+Object.freeze(defaultSetting)
 
 export const getSetting = async (): Promise<SettingOptions> => {
-  const setting: SettingOptions = await ipcRenderer.invoke('store.get', 'setting')
-  return {
-    ...defaultSetting,
-    ...setting
+  const setting: unknown = await ipcRenderer.invoke('store.get', 'setting')
+  if (!settingValidate(setting)) {
+    return defaultSetting
   }
+  return setting
 }
 
 export const setSetting = async (setting: SettingOptions): Promise<void> =>
   await ipcRenderer.invoke('store.set', 'setting', setting)
+
+export const getEncoder = (
+  encoder: SettingOptions['encoder'],
+  hardware: SettingOptions['hardware']
+): RealEncoder => {
+  switch (encoder) {
+    case 'hevc':
+      switch (hardware) {
+        case '':
+          return 'libx265'
+        case 'amd':
+          return 'hevc_amf'
+        case 'nvidia':
+          return 'hevc_nvenc'
+      }
+      break
+    case 'av1':
+      switch (hardware) {
+        case '':
+          return 'libsvtav1'
+        case 'amd':
+          return 'av1_amf'
+        case 'nvidia':
+          return 'av1_nvenc'
+      }
+  }
+}
--- src/renderer/src/views/Setting.vue
+++ src/renderer/src/views/Setting.vue
@@ -1,25 +1,32 @@
 <template>
   <NScrollbar>
-    <NForm :model="form" class="mx-2">
-      <NFormItem label="硬件编码">
+    <NForm :model="form" class="mr-4">
+      <NFormItem label="编码器">
         <div class="w-full">
           <NSelect v-model:value="form.encoder" :options="encoderOptions" />
+        </div>
+      </NFormItem>
+      <NFormItem label="硬件编码">
+        <div class="w-full">
+          <NSelect v-model:value="form.hardware" :options="hardwareOptions" />
           <NTag :bordered="false" class="w-full mt-2">
             使用硬件编码器会使图像质量稍微下降，但是可以大幅加快编码速度
           </NTag>
         </div>
       </NFormItem>
-      <NFormItem label="CRF">
+      <NFormItem label="质量因子">
         <div class="w-full">
-          <NSlider v-model:value="form.crf" :step="1" :min="0" :max="51" />
+          <NSlider v-model:value="form[reactiveForm.crf.key]" :step="1" :min="reactiveForm.crf.min"
+            :max="reactiveForm.crf.max" />
           <NTag :bordered="false" class="w-full mt-2">
-            CRF越高，生成的文件越小，但图像质量也会越低
+            质量因子越高，生成的文件越小，但图像质量也会越低
           </NTag>
         </div>
       </NFormItem>
       <NFormItem label="编码等级">
         <div class="w-full">
-          <NSlider v-model:value="form.preset" :step="1" :min="0" :max="9" />
+          <NSlider v-model:value="form[reactiveForm.preset.key]" :step="1" :min="reactiveForm.preset.min"
+            :max="reactiveForm.preset.max" />
           <NTag :bordered="false" class="w-full mt-2">
             编码等级越高，压缩比也就越高，但是编码时间会变长
           </NTag>
@@ -51,16 +58,17 @@
 </template>
 <script setup async lang="ts">
 import { NScrollbar, NForm, NFormItem, NSelect, NSlider, NTag } from 'naive-ui'
-import { reactive, watch } from 'vue'
+import { reactive, ref, watch, watchEffect } from 'vue'
 
-interface Form extends Omit<SettingOptions, 'preset'> {
-  preset: IntRange<0, 10>
-}
+const hardwareOptions = [
+  { value: '', label: '禁用' },
+  { value: 'nvidia', label: 'Nvidia' },
+  { value: 'amd', label: 'AMD' }
+]
 
 const encoderOptions = [
-  { value: 'libx265', label: '禁用' },
-  { value: 'hevc_nvenc', label: 'Nvidia' },
-  { value: 'hevc_amf', label: 'AMD' }
+  { value: 'hevc', label: 'H.265/HEVC' },
+  { value: 'av1', label: 'AV1' }
 ]
 
 const outputTypeOptions = [
@@ -75,33 +83,42 @@ const outputWebpLosslessOptions = [
   { value: 0, label: '有损' }
 ]
 
-const presetList: SettingOptions['preset'][] = [
-  'ultrafast',
-  'superfast',
-  'veryfast',
-  'faster',
-  'fast',
-  'medium',
-  'slow',
-  'slower',
-  'veryslow',
-  'placebo'
-]
-
 const setting = await window.api.getSetting()
-const form = reactive<Form>({
-  encoder: setting.encoder || 'libx265',
-  crf: setting.crf || 20,
-  preset: (presetList.findIndex((value) => value === setting.preset) as IntRange<0, 10>) || 5,
-  outputType: setting.outputType || 'original',
-  outputWebpLossless: setting.outputWebpLossless || 0,
-  outputQualityLevel: setting.outputQualityLevel || 8
+const form = reactive<SettingOptions>({ ...setting })
+watch(form, (newVal) => window.api.setSetting({ ...newVal }))
+
+interface ReactiveForm {
+  crf: {
+    key: `crf_${RealEncoder}`
+    min: 0
+    max: 51 | 62 | 255
+  }
+  preset: {
+    key: `preset_${RealEncoder}`
+    min: 0
+    max: 6 | 9 | 10 | 13
+  }
+}
+const getReactiveForm = (realEncoder: RealEncoder): ReactiveForm => ({
+  crf: {
+    key: `crf_${realEncoder}`,
+    min: 0,
+    max: window.api.settingSchema.properties[`crf_${realEncoder}`].maximum
+  },
+  preset: {
+    key: `preset_${realEncoder}`,
+    min: 0,
+    max: window.api.settingSchema.properties[`preset_${realEncoder}`].maximum
+  }
 })
 
-watch(form, (newVal) => {
-  window.api.setSetting({
-    ...newVal,
-    preset: presetList[newVal.preset]
-  })
+const { encoder, hardware } = form
+const realEncoder = window.api.getEncoder(encoder, hardware)
+const reactiveForm = ref<ReactiveForm>(getReactiveForm(realEncoder))
+
+watchEffect(() => {
+  const { encoder, hardware } = form
+  const realEncoder = window.api.getEncoder(encoder, hardware)
+  reactiveForm.value = getReactiveForm(realEncoder)
 })
 </script>
